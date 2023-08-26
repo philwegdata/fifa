f00883d083cd68b89f8fbf01431d023c70ec415c
phil <63159744+philwegdata@users.noreply.github.com>
2023-08-26 17:32:57 +0200

    Update top5_app.py

--- top5_app.py
+++ top5_app.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.express as px
 import plotly.graph_objects as go
 
 # Load the preprocessed CSV file with average overall scores
